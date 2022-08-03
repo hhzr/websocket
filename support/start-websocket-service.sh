b6a2b5af1b147d812f31a02c08e171fc3060c87b
Sun <hhzr9920@163.com>
2022-08-03 22:43:56 +0800

    启动脚本

--- support/start-websocket-service.sh
+++ support/start-websocket-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SERVER_NAME="websocket-service"
-JAR_FILE="/opt/app/crm-service.jar"
+JAR_FILE="/opt/app/websocket_demo-0.0.1-SNAPSHOT.jar"
 CUSTOMER="  "
 ./start.sh $SERVER_NAME $JAR_FILE $CUSTOMER
